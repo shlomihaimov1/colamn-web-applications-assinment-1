ccb5216f2662220024d98e4d335592412b85a365
elad32011 <elad32011@gmail.com>
2025-01-18 14:01:31 +0200

    auth controller, middleware

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,14 +9,78 @@ import { Document } from 'mongoose';
 // Handle user operations
 
 const register = async (req: Request, res: Response) => {
+    try {
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const user = await User.create({
+            email: req.body.email,
+            username: req.body.username,
+            password: hashedPassword,
+        });
+        
+        res.status(200).send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 };
 
 const login = async (req: Request, res: Response) => {
-   
+    try {
+        
+        const user = await User.findOne({ email: req.body.email });
+        
+        if (!user) {
+            res.status(400).send('wrong username or password');
+            return;
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        
+        if (!validPassword) {
+            res.status(400).send('wrong username or password');
+            return;
+        }
+        
+        if (!process.env.TOKEN_SECRET) {
+            res.status(500).send('Server Error');
+            return;
+        }
+
+        // Generate a token
+        const tokens = generateToken(user._id);
+        if (!tokens) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        if (!user.refreshToken) {
+            user.refreshToken = [];
+        }
+
+        user.refreshToken.push(tokens.refreshToken);
+        await user.save();
+
+        res.status(200).send({
+            accessToken: tokens.accessToken,
+            refreshToken: tokens.refreshToken,
+            _id: user._id
+        });
+
+    } catch (err) {
+        res.status(400).send(err);
+    }
 };
 
 const logout = async (req: Request, res: Response) => {
-   
+    try {
+        const user = await verifyRefreshToken(req.body.refreshToken);
+        await user.save();
+
+        res.status(200).send("success");
+    } catch (err) {
+        res.status(400).send("fail");
+    }
 };
 
 
@@ -53,7 +117,33 @@ const generateToken = (userId: string): tTokens | null => {
 };
 
 const refresh = async (req: Request, res: Response) => {
+    try {
+        const user = await verifyRefreshToken(req.body.refreshToken);
+        if (!user) {
+            res.status(400).send("fail");
+            return;
+        }
+        const tokens = generateToken(user._id);
 
+        if (!tokens) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        if (!user.refreshToken) {
+            user.refreshToken = [];
+        }
+        user.refreshToken.push(tokens.refreshToken);
+        await user.save();
+        res.status(200).send(
+            {
+                accessToken: tokens.accessToken,
+                refreshToken: tokens.refreshToken,
+                _id: user._id
+            });
+        //send new token
+    } catch (err) {
+        res.status(400).send("fail");
+    }
 };
 
 type tUser = Document<unknown, {}, IUser> & IUser & Required<{
@@ -62,7 +152,54 @@ type tUser = Document<unknown, {}, IUser> & IUser & Required<{
     __v: number;
 }
 const verifyRefreshToken = (refreshToken: string | undefined) => {
+    return new Promise<tUser>((resolve, reject) => {
+        
+        // Get refresh token from body
+        if (!refreshToken) {
+            reject("fail");
+            return;
+        }
+        
+        // Verify token
+        if (!process.env.TOKEN_SECRET) {
+            reject("fail");
+            return;
+        }
 
+        jwt.verify(refreshToken, process.env.TOKEN_SECRET, async (err: any, payload: any) => {
+            if (err) {
+                reject("fail");
+                return
+            }
+
+            // Get the user id fromn token
+            const userId = payload._id;
+            try {
+                // Get the user form the db
+                const user = await User.findById(userId);
+                
+                if (!user) {
+                    reject("fail");
+                    return;
+                }
+                
+                if (!user.refreshToken || !user.refreshToken.includes(refreshToken)) {
+                    user.refreshToken = [];
+                    await user.save();
+                    reject("fail");
+                    return;
+                }
+                
+                const tokens = user.refreshToken!.filter((token) => token !== refreshToken);
+                user.refreshToken = tokens;
+
+                resolve(user);
+            } catch (err) {
+                reject("fail");
+                return;
+            }
+        });
+    });
 }
 
 
